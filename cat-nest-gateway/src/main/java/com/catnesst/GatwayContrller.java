dd897a35f1294ad689cf43bad40d17aa54cccf0c
Yiwyn <5597104@qq.com>
2023-08-11 23:06:46 +0800

    gateway 构建

---
+++ cat-nest-gateway/src/main/java/com/catnesst/GatwayContrller.java
@@ -0,0 +1,13 @@
+package com.catnesst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayContrller {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayContrller.class, args);
+    }
+
+}
