9f1225d6f36b70893e5b245da999f402fc3e01df
Yiwyn <5597104@qq.com>
2023-08-27 01:44:34 +0800

    多线程执行事件，创建事件添加redis

---
+++ cat-nest-modules/cat-nest-system/src/main/java/com/catnest/system/config/TaskConfig.java
@@ -0,0 +1,29 @@
+package com.catnest.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class TaskConfig {
+
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        //设置线程池参数信息
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(50);
+        taskExecutor.setQueueCapacity(200);
+        taskExecutor.setKeepAliveSeconds(60);
+        taskExecutor.setThreadNamePrefix("myExecutor--");
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setAwaitTerminationSeconds(60);
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+}
--- cat-nest-modules/cat-nest-system/src/main/java/com/catnest/system/listener/CatNestListener.java
+++ cat-nest-modules/cat-nest-system/src/main/java/com/catnest/system/listener/CatNestListener.java
@@ -2,8 +2,12 @@ package com.catnest.system.listener;
 
 import com.catnest.system.domain.CatNestRecord;
 import com.catnest.system.event.CatNestCreateEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +16,18 @@ import org.springframework.stereotype.Component;
 public class CatNestListener {
 
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+
+    //todo  magic-value need deal
     @Async
     @EventListener
-    public void onCreateCatNest(CatNestCreateEvent catNestCreateEvent) {
+    public void onCreateCatNest(CatNestCreateEvent catNestCreateEvent) throws JsonProcessingException {
         CatNestRecord catNestRecord = catNestCreateEvent.getCatNestRecord();
-        log.info("创建猫窝成功，执行事件;猫窝:{}", catNestRecord);
+        log.info("创建猫窝成功，执行事件;猫窝信息:{}", catNestRecord);
+        ObjectMapper om = new ObjectMapper();
+        redisTemplate.boundHashOps("latest_create").put(String.valueOf(catNestRecord.getNestId()), om.writeValueAsString(catNestRecord));
     }
 
 
