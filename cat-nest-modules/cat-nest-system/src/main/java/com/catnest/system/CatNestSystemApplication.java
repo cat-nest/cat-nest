0fd747cdaf415e29b9a647b2aec2e8913d2f088e
Yiwyn <5597104@qq.com>
2023-08-27 17:38:44 +0800

    maven结构梳理

--- cat-nest-modules/cat-nest-system/src/main/java/com/catnest/system/CatNestSystemApplication.java
+++ cat-nest-modules/cat-nest-system/src/main/java/com/catnest/system/CatNestSystemApplication.java
@@ -4,7 +4,9 @@ package com.catnest.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @MapperScan("com.catnest.system.mapper")
 public class CatNestSystemApplication {
