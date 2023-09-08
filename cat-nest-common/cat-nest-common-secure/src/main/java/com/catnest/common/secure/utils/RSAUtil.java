e9d4737ced51c082a8a1749cdc853945d1f82c25
yiwyn <5597104@qq.com>
2023-09-08 11:11:31 +0800

    + RSAUtil ras加解密工具完善

--- cat-nest-common/cat-nest-common-secure/src/main/java/com/catnest/common/secure/utils/RSAUtil.java
+++ cat-nest-common/cat-nest-common-secure/src/main/java/com/catnest/common/secure/utils/RSAUtil.java
@@ -1,9 +1,122 @@
 package com.catnest.common.secure.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Cipher;
+import java.io.ByteArrayOutputStream;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Slf4j
 public class RSAUtil {
 
 
+    public static final String KEY_ALGORITHM = "RSA";
 
+    /**
+     * RSA最大加密明文大小
+     */
+    private static final int MAX_ENCRYPT_BLOCK = 245;
+    /**
+     * RSA最大解密密文大小
+     */
+    private static final int MAX_DECRYPT_BLOCK = 256;
+
+
+    /**
+     * 获取公钥
+     */
+    public static PublicKey getPublicKey(String publicKeyString) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] publicKeyByte = Base64.getDecoder().decode(publicKeyString);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyByte);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        return keyFactory.generatePublic(keySpec);
+    }
+
+
+    /**
+     * 获取私钥
+     */
+    public static PrivateKey getPrivateKey(String privateKeyString) throws Exception {
+        byte[] privateKeyByte = Base64.getDecoder().decode(privateKeyString);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyByte);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+
+    /**
+     * 加密工具
+     *
+     * @param plainText    需要加密内容
+     * @param publicKeyStr 公钥
+     * @return
+     * @throws Exception
+     */
+    public static String encrypt(String plainText, String publicKeyStr) throws Exception {
+
+        byte[] plainTextArray = plainText.getBytes();
+        PublicKey publicKey = getPublicKey(publicKeyStr);
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        int inputLen = plainTextArray.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        int i = 0;
+        byte[] cache;
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(plainTextArray, offSet, MAX_ENCRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(plainTextArray, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_ENCRYPT_BLOCK;
+        }
+        byte[] encryptText = out.toByteArray();
+        out.close();
+        return Base64.getEncoder().encodeToString(encryptText);
+    }
+
+
+    /**
+     * 解密工具
+     *
+     * @param encryptTextHex 解密
+     * @param privateKeyStr  私钥
+     * @return 解密明文
+     */
+    public static String decrypt(String encryptTextHex, String privateKeyStr) throws Exception {
+        byte[] encryptText = Base64.getDecoder().decode(encryptTextHex);
+        PrivateKey privateKey = getPrivateKey(privateKeyStr);
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        int inputLen = encryptText.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // 对数据分段解密
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_DECRYPT_BLOCK) {
+                cache = cipher.doFinal(encryptText, offSet, MAX_DECRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(encryptText, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_DECRYPT_BLOCK;
+        }
+        out.close();
+        return out.toString();
+    }
 
 
 }
