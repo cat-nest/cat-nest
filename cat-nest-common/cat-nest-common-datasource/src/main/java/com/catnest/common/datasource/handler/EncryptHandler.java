ff8b171325069d534be9b39b93f769dba18bc1f8
Yiwyn <5597104@qq.com>
2023-09-13 23:51:36 +0800

    + 替换mybatis 为 mybatis plus 添加加密handler

---
+++ cat-nest-common/cat-nest-common-datasource/src/main/java/com/catnest/common/datasource/handler/EncryptHandler.java
@@ -0,0 +1,46 @@
+package com.catnest.common.datasource.handler;
+
+import com.baomidou.mybatisplus.core.toolkit.AES;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * 参考链接
+ * <a href="https://mybatis.org/mybatis-3/zh/configuration.html#typeHandlers">MyBatis Handers</a>
+ */
+public class EncryptHandler extends BaseTypeHandler<String> {
+
+    @Value("${key}")
+    public String key;
+
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, String s, JdbcType jdbcType) throws SQLException {
+        String encrypt = AES.encrypt(s, key);
+        preparedStatement.setString(i, encrypt);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        String columnValue = resultSet.getString(s);
+        return AES.decrypt(columnValue, key);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        String columnValue = resultSet.getString(i);
+        return AES.decrypt(columnValue, key);
+    }
+
+    @Override
+    public String getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        String columnValue = callableStatement.getString(i);
+        return AES.decrypt(columnValue, key);
+    }
+}
