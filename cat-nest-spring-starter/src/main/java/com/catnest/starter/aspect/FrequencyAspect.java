238ffccbfaedea0142ff10a317aab6ee3ede870d
Yiywn <5597104@qq.com>
2023-09-19 10:37:33 +0800

    + 接口频率控制

---
+++ cat-nest-spring-starter/src/main/java/com/catnest/starter/aspect/FrequencyAspect.java
@@ -0,0 +1,31 @@
+package com.catnest.starter.aspect;
+
+import com.catnest.starter.annontation.FrequencyCtrl;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * 频率控制切面
+ * todo 待完善
+ */
+@Aspect
+@Slf4j
+public class FrequencyAspect {
+
+
+    @Pointcut("@annotation(frequencyCtrl)")
+    public void Pointcut(FrequencyCtrl frequencyCtrl) {
+    }
+
+    @Around(value = "Pointcut(frequencyCtrl)", argNames = "pjp,frequencyCtrl")
+    public Object around(ProceedingJoinPoint pjp, FrequencyCtrl frequencyCtrl) throws Throwable {
+        //限制时间
+        long period = frequencyCtrl.period();
+        //访问次数
+        long count = frequencyCtrl.count();
+        return pjp.proceed();
+    }
+}
