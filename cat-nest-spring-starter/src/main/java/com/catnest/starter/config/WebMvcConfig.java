c06a7fc37b336a6be46b4a23728808d2ad0bfc22
Yiywn <5597104@qq.com>
2023-09-19 10:09:39 +0800

    + 文件名修正

--- cat-nest-spring-starter/src/main/java/com/catnest/starter/config/WebMvcConfig.java
+++ cat-nest-spring-starter/src/main/java/com/catnest/starter/config/WebMvcConfig.java
@@ -1,7 +1,7 @@
 package com.catnest.starter.config;
 
 
-import com.catnest.starter.interceptor.TranceInterceptor;
+import com.catnest.starter.interceptor.TraceIdInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TranceInterceptor());
+        registry.addInterceptor(new TraceIdInterceptor());
         WebMvcConfigurer.super.addInterceptors(registry);
     }
 
