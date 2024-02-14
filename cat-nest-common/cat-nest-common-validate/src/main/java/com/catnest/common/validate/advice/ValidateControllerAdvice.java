fc049d886814bd4b7c73f07fb64ba43ebfbb3c19
Yiwyn <5597104@qq.com>
2024-02-14 15:32:11 +0800

    校验工具

--- cat-nest-common/cat-nest-common-validate/src/main/java/com/catnest/common/validate/advice/ValidateControllerAdvice.java
+++ cat-nest-common/cat-nest-common-validate/src/main/java/com/catnest/common/validate/advice/ValidateControllerAdvice.java
@@ -1,12 +1,13 @@
 package com.catnest.common.validate.advice;
 
 
+import com.catnest.common.core.domain.ApiResponse;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -15,39 +16,39 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ValidateControllerAdvice {
     /**
      * 校验异常
      */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public  exceptionHandler(MethodArgumentNotValidException e) {
+    public ApiResponse<Object> exceptionHandler(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
-        String errorMesssage = "";
+        StringBuilder errorMesssage = new StringBuilder();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            errorMesssage += fieldError.getDefaultMessage() + "!";
+            errorMesssage.append(fieldError.getDefaultMessage());
         }
-        return AjaxResult.error(errorMesssage);
+        return ApiResponse.doFail(errorMesssage.toString());
     }
 
     /**
      * 校验异常
      */
     @ExceptionHandler(value = BindException.class)
-    public AjaxResult validationExceptionHandler(BindException e) {
+    public ApiResponse<Object> validationExceptionHandler(BindException e) {
         BindingResult bindingResult = e.getBindingResult();
-        String errorMesssage = "";
+        StringBuilder errorMesssage = new StringBuilder();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            errorMesssage += fieldError.getDefaultMessage() + "!";
+            errorMesssage.append(fieldError.getDefaultMessage());
         }
-        return AjaxResult.error(errorMesssage);
+        return ApiResponse.doFail(errorMesssage.toString());
     }
 
     /**
      * 校验异常
      */
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public AjaxResult ConstraintViolationExceptionHandler(ConstraintViolationException ex) {
+    public ApiResponse<Object> ConstraintViolationExceptionHandler(ConstraintViolationException ex) {
         Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
         Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
         List<String> msgList = new ArrayList<>();
@@ -55,8 +56,7 @@ public class ValidateControllerAdvice {
             ConstraintViolation<?> cvl = iterator.next();
             msgList.add(cvl.getMessageTemplate());
         }
-        return AjaxResult.error(String.join(",", msgList));
+        return ApiResponse.doFail(String.join(",", msgList));
     }
-
 
 }
